e827d16816ad7c759b5d7f47036c0a433704f042
Bao143 <taodiepgiabao070414@gmail.com>
2026-04-15 20:47:02 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCMenuItemSprite.hpp>
+#include <Geode/modify/CCMenuItemSpriteExtra.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MyButtonHook, CCMenuItemSprite) {
-    void selected() {
-        CCMenuItemSprite::selected();
-
+class $modify(CCMenuItemSpriteExtra) {
+    void activate() {
+        CCMenuItemSpriteExtra::activate();
         FMODAudioEngine::sharedEngine()->playEffect("honk.wav");
+        auto texCache = CCTextureCache::sharedTextureCache();
+        auto newTexture = texCache->addImage("soggycat.png", false);
 
-        if (auto sprite = typeinfo_cast<CCSprite*>(this->getNormalImage())) {
-            auto texCache = CCTextureCache::sharedTextureCache();
-            auto newTexture = texCache->addImage("soggycat.png", false);
-
-            if (newTexture) {
+        if (newTexture) {
+            if (auto sprite = static_cast<CCSprite*>(this->m_normalImage)) {
                 sprite->setTexture(newTexture);
+                auto size = newTexture->getContentSize();
+                sprite->setTextureRect({ 0, 0, size.width, size.height });
+                this->updateSprite();
             }
+        } else {
+            log::error("cant find soggy.png, check resources pls");
         }
     }
 };
